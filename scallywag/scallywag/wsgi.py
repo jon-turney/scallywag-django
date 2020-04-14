cc0c904f5c528ef449f5c6051e8e67717fe3f56e
Jon Turney <jon.turney@dronecode.org.uk>
2020-04-14 20:26:59 +0100

    Ensure this django project is on pythonpath for wsgi
    Ensure this django project is on pythonpath for wsgi, for running in environments
    where we can't set the pythonpath directly.

--- scallywag/scallywag/wsgi.py
+++ scallywag/scallywag/wsgi.py
@@ -7,9 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+import sys
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+# ensure the project is in the pythonpath
+sys.path.append(os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scallywag.settings")
 
